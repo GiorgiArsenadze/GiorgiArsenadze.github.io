ee7d13ac0ac94a03b422e79014648e208f0e8cec
GiorgiArsenadze <54861034+GiorgiArsenadze@users.noreply.github.com>
2025-08-13 23:54:08 +0000

    Deploying to gh-pages from @ GiorgiArsenadze/GiorgiArsenadze.github.io@48f9458e822dcfb96ee42aa23b65bd6339937b24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,8 +38,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "My teaching and lecture materials.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
